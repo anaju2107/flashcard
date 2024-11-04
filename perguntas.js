5451568021c80fdd6bc0db458ebc729569401bd7
anaju2107 <00001085388736sp@al.educacao.sp.gov.br>
2024-11-04 10:59:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    '  medicina veterinária ',
+    ' o que a medicina veterinária estuda?',
+    ' anatomia, microbiologia, genética, nutrição e produção animal, bem como matemática, estatística e bioética. Nos anos seguintes as disciplinas focam em doenças e técnicas clínicas e cirúrgicas.'
+)
+
+criaCartao(
+    'Faculdades',
+    'quais são as principais áreas da medicina veterinária',
+    'Clínica de grandes animais, Cirurgia de grandes animaisCirurgia em médios animais,Cirurgia em animais de companhia, Cirurgia de animais selvagens,Cirurgia de aves, cirurgia de peixes.')
+
+criaCartao(
+    'Custo',
+    'quanto custa a fauldade de medicina veterinária?',
+    'a mensalidade de Medicina custa, em média, R$ 7.300. '
+)
+
+criaCartao(
+    'Àrea escolhida',
+    'Qual Área e mais bem paga na medicina veterinária?',
+    'Perícia Técnica'
+)
